d928a3b323e6d34b2af649d918469ec035e0b3d4
Irina Mishina <irinamishina131@gmail.com>
2025-02-26 18:14:58 -0500

    Continuous music in the menus

---
+++ Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DontDestroy : MonoBehaviour
+{
+    AudioSource music;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        music = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(SceneManager.GetActiveScene().name != "MainMenu" && SceneManager.GetActiveScene().name != "HelpMenu" && SceneManager.GetActiveScene().name != "DifficultyMenu" && SceneManager.GetActiveScene().name != "CreditsMenu")
+        {
+            music.volume = 0;
+        }
+    }
+
+    void Awake()
+    {
+        GameObject[] obj = GameObject.FindGameObjectsWithTag("Music");
+        if(obj.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
